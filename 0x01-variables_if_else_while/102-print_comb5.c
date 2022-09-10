3c2545783ac45ca389b71d4e1362619d2c5d86b1
Emmanuelojo38 <lanremj@gmail.com>
2022-09-10 16:10:55 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,24 +9,33 @@
  */
 int main(void)
 {
-	int no1, no2;
+	int a, b, c, d;
+	init i, j;
 
-	for (no1 = 0; no1 <= 98; no1++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		for(no2 = no1 + 1; no2 <= 99; no2++)
+		for (b = '0'; b <= '9'; b++)
 		{
-			putchar((no1 / 10) + '0');
-			putchar((no1 % 10) + '0');
-			putchar('\n');
-			putchar((no2 /10) + '0');
-			putchar((no2 % 10) + '0');
-			
-			if (no1 == 98 && no2 == 99)
-				continue;
-			putchar(',');
-			putchar(' ');
+			i = a;
+			b++;
+			j = b;
+			b--;
+			for (c = '0'; c <= '9'; c++)
+			{
+				for (d = j; d <= '9'; d++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+					if (a == '9' && b == '8' && c == '9' && d == '9')
+						break;
+					putchar (',');
+					putchar (' ');
+				}
+			}
 		}
 	}
-	putchar('\n');
 	return (0);
 }
