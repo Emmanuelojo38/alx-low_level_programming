5ee8a8b0ca0718b5f3d1be470d3dd16bf997727c
Emmanuelojo38 <lanremj@gmail.com>
2022-09-09 21:50:53 -0700

    task 6

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int no;
+	for (no = '0'; no <= '9'; n++)
+	{
+		putchar(no);
+	}
+putchar('\n');
+	return (0);
+}
